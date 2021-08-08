86ad319a94c98586d0cac43599d7062ff962cb62
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-08-08 10:36:58 +0200

    base && git push && git remote add main https://github.com/AlexisVS/React-JSX-exo4.git && git push main && git push --set-upstream main master

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render () {
+    return (
+      <>
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+      <i class="fab fa-500px"></i>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import "./sass/app.sass";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "@fortawesome/fontawesome-free/js/all.min.js";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+
